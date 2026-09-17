c627ad56b7436cc438ede95f1153c6395c0e414f
Ploichompoo Saewa <ploychompoo0802@gmail.com>
2026-09-17 15:20:18 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+// Google Analytics 4 Script
+const script1 = document.createElement('script');
+script1.async = true;
+script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-KNZ6KBCG48';
+document.head.appendChild(script1);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KNZ6KBCG48');
 import './styles/global.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
