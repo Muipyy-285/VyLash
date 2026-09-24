487c469251595331cd5bd74772233bb6b60e5658
Ploichompoo Saewa <ploychompoo0802@gmail.com>
2026-09-24 16:07:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
-import Header from './components/Header';
 import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Product from './pages/Product';
 import TryOn from './pages/TryOn';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Feedback from './pages/Feedback';
 
 function App() {
   return (
-    <CartProvider>
-      <Router basename="/VyLash">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="/try-on" element={<TryOn />} />
-            <Route path="/feedback" element={<Feedback />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </Router>
-    </CartProvider>
+    <Router basename="/VyLash">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/try-on" element={<TryOn />} />
+      </Routes>
+    </Router>
   );
 }
 
